d7be4d4df53898b7c9e40352fd321c0b0fad306f
fattaneh <fatameri@hotmail.com>
2017-06-30 16:38:32 -0400

    console

--- CodeRanking/Program.cs
+++ CodeRanking/Program.cs
@@ -10,6 +10,23 @@ namespace CodeRanking
     {
         static void Main(string[] args)
         {
+            double mealCost = Console.Read();
+            Console.ReadLine();
+            int tipPercent = Console.Read();
+            Console.ReadLine();
+            int taxPercent = Console.Read();
+            Console.ReadLine();
+
+            tipPercent= (int) (mealCost * (tipPercent / 100));
+            taxPercent = (int) (mealCost * (taxPercent / 100));
+            var totalCost = (int)mealCost + taxPercent + tipPercent;
+
+            Console.WriteLine(totalCost);
+            Console.ReadLine();
+
+            //totalCost = (int)Math.Round(totalCost + 0.5);
+            //Console.WriteLine(totalCost);
+            //Console.ReadLine();
         }
     }
 }
